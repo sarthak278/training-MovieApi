900d1e725e7963bbff611129bfc9e71aaf4aba47
sarthak278 <99467343+sarthak278@users.noreply.github.com>
2022-05-31 17:35:43 +0530

    Add files via upload

---
+++ MyMovieApp.Entity/ThetreModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyMovieApp.Entity
+{
+    public class ThetreModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TheaterId { get; set; }
+        public string TheaterName { get; set; }
+
+        public string TheaterMovies { get; set; }
+
+        public string TheaterAddress { get; set; }
+
+    }
+}
